c0d16a7d58a49d80e2c4976e3a92743a24684bd5
Jia-Shen Boon <jiashen@gmail.com>
2016-03-26 19:46:42 +0000

    Explain gunicorn

--- 2_gunicorn_locally_launched_hello_world/run_locally.sh
+++ 2_gunicorn_locally_launched_hello_world/run_locally.sh
@@ -1,2 +1,4 @@
-gunicorn -b 0.0.0.0:5000 hello:app
+# Where the python module is hello.py and app is the python flask variable in the module
+gunicorn -b 0.0.0.0:5000 hello:app  
+
 #gunicorn hello:app  # if you don't want to specify host/port
